9fb106df35290d7fbc02aaf6d9b1ddb2d9efaec1
คีตศิลป์ คงสี <keetasin01596@gmail.com>
2025-10-08 21:54:48 +0700

    main

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ with app.app_context():
 
     start_stock_scheduler(app)
     start_news_scheduler(app)
-    start_forecast_scheduler(app)
+    # start_forecast_scheduler(app)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
